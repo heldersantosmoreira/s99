d8c599d9e9c865abb11cc9c60a0d010246abd42f
Hélder Moreira <heldersantos.moreira@gmail.com>
2016-10-07 15:25:59 +0100

    Start on Logic and Codes

--- src/main/scala/s99/LogicAndCodesSolutions.scala
+++ src/main/scala/s99/LogicAndCodesSolutions.scala
@@ -13,17 +13,23 @@ trait LogicAndCodesSolutions { outer =>
     def impl(b: => Boolean): Boolean = outer.impl(a, b)
     def equ(b: => Boolean): Boolean = outer.equ(a, b)
   }
-  
-  def and(a: Boolean, b: => Boolean): Boolean = ???
-  def or(a: Boolean, b: => Boolean): Boolean = ???
-  def nand(a: Boolean, b: => Boolean): Boolean = ???
-  def nor(a: Boolean, b: => Boolean): Boolean = ???
-  def xor(a: Boolean, b: => Boolean): Boolean = ???
-  def impl(a: Boolean, b: => Boolean): Boolean = ???
-  def equ(a: Boolean, b: => Boolean): Boolean = ???
-  def not(a: Boolean): Boolean = ???
 
-  def table2(f: (Boolean, Boolean) => Boolean): String = ???
+  def and(a: Boolean, b: => Boolean): Boolean = if (a) b else false
+  def or(a: Boolean, b: => Boolean): Boolean = if (a) true else b
+  def nand(a: Boolean, b: => Boolean): Boolean = not(and(a, b))
+  def nor(a: Boolean, b: => Boolean): Boolean = not(or(a, b))
+  def xor(a: Boolean, b: => Boolean): Boolean = a != b
+  def impl(a: Boolean, b: => Boolean): Boolean = if (a) b else true
+  def equ(a: Boolean, b: => Boolean): Boolean = not(xor(a, b))
+  def not(a: Boolean): Boolean = if (a) false else true
+
+  def table2(f: (Boolean, Boolean) => Boolean): String = {
+    def options = List((true, true), (true, false), (false, true), (false, false))
+    def toString(b: Boolean) = if (b) "true  " else "false "
+    ("A     B     result" :: options.map {
+      case (a, b) => List(a, b, f(a, b)).map(toString).mkString("")
+    }).mkString("\n")
+  }
 
   def gray(n: Int): List[String] = ???
   def huffman(list: List[(String,  Int)]): List[(String, String)] = ???
